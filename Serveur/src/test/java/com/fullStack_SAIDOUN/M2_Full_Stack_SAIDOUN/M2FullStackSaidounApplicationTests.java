6c4b8c74b584859cdfd38c2369a2321e5fcd60d2
Abdelkrim <saidoun.abdelkrim@outlook.com>
2024-09-10 10:03:26 +0200

    FS_K: initialise serveur

---
+++ Serveur/src/test/java/com/fullStack_SAIDOUN/M2_Full_Stack_SAIDOUN/M2FullStackSaidounApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStack_SAIDOUN.M2_Full_Stack_SAIDOUN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2FullStackSaidounApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
