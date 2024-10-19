8be42fb4b035c519de2808e8493a81b842bfc8d3
codeNet <158341443+codeNetSolution@users.noreply.github.com>
2024-10-19 10:33:53 +0200

    addroutes in app

--- FullStackM2/Frontend/management-category/src/app/app.routes.ts
+++ FullStackM2/Frontend/management-category/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CategoryFormComponent } from './components/category-form/category-form.component';
+import { CategoryDetailsComponent } from './components/category-details/category-details.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/categories', pathMatch:'full'},
+    { path: 'categories', component: CategoryListComponent},
+    { path: 'category/new', component: CategoryFormComponent},
+    { path: 'category/edit/:id', component: CategoryFormComponent},
+    { path: 'category/:id', component: CategoryDetailsComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
